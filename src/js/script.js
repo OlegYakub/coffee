5624ff92ee6141e204d9025130112fde44e46638
vlad <vladkuraev84@gmail.com>
2016-11-10 09:12:50 +0200

    com 0911

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+﻿'use strict'
+$(document).ready(function(){
+
+	$("#slider").slider({
+      min: 0,
+		  max: 350,
+		  value: 115,
+		  range: "min",
+		  stop: function(event, ui) {
+	      $("input#maxCost").val($("#slider").slider("values",0));
+		  },
+		  slide: function(event, ui){
+	      $("input#maxCost").val($("#slider").slider("values",0));
+		  }
+	});
+	$("#slider_cup").slider({
+      min: 0,
+		  max: 50,
+		  value: 5,
+		  range: "min",
+		  stop: function(event, ui) {
+	      $("input#maxCost_cup").val($("#slider_cup").slider("values",0));
+		  },
+		  slide: function(event, ui){
+	      $("input#maxCost_cup").val($("#slider_cup").slider("values",0));
+		  }
+	});
+
+});
